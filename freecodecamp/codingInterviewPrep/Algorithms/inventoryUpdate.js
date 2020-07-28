ba1499cf079bff74477a39e728766dd01d491e8c
Nicanor008 <nicanorkorir008@gmail.com>
2020-07-28 18:33:13 +0300

    Freecodecamp and interview question DSA

---
+++ freecodecamp/codingInterviewPrep/Algorithms/inventoryUpdate.js
@@ -0,0 +1,45 @@
+// Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1).
+// If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
+
+function updateInventory(arr1, arr2) {
+  let newArr = [];
+
+  //   get all product names
+  for (let i = 0; i < arr1.length; i++) {
+    newArr.push(arr1[i][1]);
+  }
+
+  //   check on arr 2 if it exists in arr 1
+  for (let i = 0; i < arr2.length; i++) {
+    if (!newArr.includes(arr2[i][1])) {
+      arr1.push(arr2[i]);
+    } else {
+      var index = newArr.indexOf(arr2[i][1]); // get the index
+      arr1[index] = arr1[index][0] + arr2[i][0]; //get quantity
+    }
+  }
+
+  //   sort the result
+  arr1.sort((a, b) => {
+    if (a[1] > b[1]) return 1;
+    if (a[1] < b[1]) return -1;
+    return 0;
+  });
+  return arr1;
+}
+
+var curInv = [
+  [21, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [1, "Hair Pin"],
+  [5, "Microphone"],
+];
+
+var newInv = [
+  [2, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [67, "Bowling Ball"],
+  [7, "Toothpaste"],
+];
+
+console.log(updateInventory(curInv, newInv));
---
+++ freecodecamp/codingInterviewPrep/Algorithms/symmetricDiff.js
@@ -0,0 +1,28 @@
+function sym(args) {
+  // The slice() method is used to break down the arguments object to an array, args.
+  var args = Array.prototype.slice.call(arguments);
+
+  // get the difference
+  // The getDiff function finds the symmetric difference between two sets, arr1 and arr2. It is used as a callback function for the reduce() method called on args.
+  var getDiff = (a1, a2) => {
+    // return items in a1 that don't exist in a2
+    function filterFunction(a1, a2) {
+      return a1.filter((item) => {
+        return a2.indexOf(item) === -1;
+      });
+    }
+
+    // compare all arrays
+    return filterFunction(a1, a2).concat(filterFunction(a2, a1));
+  };
+
+  var summary = args.reduce(getDiff, []);
+  // get the unique values
+  var unique = summary.filter((elem, index, self) => {
+    return (index === self.indexOf(elem));
+  });
+
+  return unique;
+}
+
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
